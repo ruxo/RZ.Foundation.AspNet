72611a1939e0f0e6719b7e4f1239ae3e21349cbc
Ruxo Zheng <rz@just.sent.as>
2024-12-07 05:29:14 +0700

    Use clipped menu style. Use responsive by default.

--- src/RZ.Foundation.Blazor.MudBlazor/BlazorViews/Shells/ShellNavMenu.razor.cs
+++ src/RZ.Foundation.Blazor.MudBlazor/BlazorViews/Shells/ShellNavMenu.razor.cs
@@ -28,7 +28,7 @@ public class ShellNavMenuViewModel : ViewModel, IDisposable
                               .ToProperty(this, x => x.IsDrawerVisible)
                               .DisposeWith(disposables);
         variant = shell.WhenAnyValue(x => x.UseMiniDrawer)
-                       .Select(x => x ? DrawerVariant.Mini : DrawerVariant.Persistent)
+                       .Select(x => x ? DrawerVariant.Mini : DrawerVariant.Responsive)
                        .ToProperty(this, x => x.Variant)
                        .DisposeWith(disposables);
         showOnHover = shell.WhenAnyValue(x => x.UseMiniDrawer)
