b90ac1a07fb103afb533f2a59ba86e47707ba7ab
Ruxo Zheng <rz@just.sent.as>
2024-12-26 18:45:56 +0700

    Allow using static files in non-prod environments.

--- src/RZ.AspNet.Bootstrapper/Blazor/AssetMapModule.cs
+++ src/RZ.AspNet.Bootstrapper/Blazor/AssetMapModule.cs
@@ -1,7 +1,15 @@
-﻿namespace RZ.AspNet.Blazor;
+﻿using Microsoft.AspNetCore.Hosting;
 
-public class AssetMapModule : AppModule
+namespace RZ.AspNet.Blazor;
+
+public class AssetMapModule(bool useAssetInAllNonProdEnvironments = true) : AppModule
 {
+    public override ValueTask<Unit> InstallServices(IHostApplicationBuilder builder) {
+        if (useAssetInAllNonProdEnvironments && !builder.Environment.IsDevelopment() && !builder.Environment.IsProduction())
+            (builder as WebApplicationBuilder)?.WebHost.UseStaticWebAssets();
+        return base.InstallServices(builder);
+    }
+
     public override ValueTask<Unit> InstallMiddleware(IHostApplicationBuilder configuration, WebApplication app) {
         app.MapStaticAssets();
         return base.InstallMiddleware(configuration, app);
