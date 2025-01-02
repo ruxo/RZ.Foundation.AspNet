fecc0b7bad179339432316b2be3995b7fa7ec5b4
Ruxo Zheng <rz@just.sent.as>
2025-01-02 08:17:06 +0700

    Add FailWith and Theme.

---
+++ src/RZ.AspNet.Bootstrapper/Helper/FailWithExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+// ReSharper disable TemplateIsNotCompileTimeConstantProblem
+
+namespace RZ.AspNet.Helper;
+
+[PublicAPI]
+public static class FailWithExtensions
+{
+    [DoesNotReturn]
+    public static T FailWith<T>(this ILogger logger, string message) {
+        logger.LogCritical(message);
+        Environment.FailFast(message);
+        return default!;
+    }
+}
