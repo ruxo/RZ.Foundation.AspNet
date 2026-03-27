3e1d408e6c9d181ab9efc6720846b2f152b4ed31
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-03-27 20:26:31 +0700

    Fix race condition with trackBlur

--- src/RZ.Foundation.Blazor.MudBlazor/BlazorViews/ShellNavMenu.razor.js
+++ src/RZ.Foundation.Blazor.MudBlazor/BlazorViews/ShellNavMenu.razor.js
@@ -1,4 +1,5 @@
 export function trackBlur(element, menuView){
+    if (!element) return  // prevent race-condition from IsDrawerVisible state change during calling this JS function
     element.addEventListener('focusout', event => {
         if (!element.contains(event.relatedTarget))
             menuView.invokeMethodAsync('OnBlur');
